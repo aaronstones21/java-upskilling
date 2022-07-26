4267e4f6249813de5e87a810d34d3443f331b064
Aaron Stones <aaronstones@macbook-pro.home>
2022-07-26 11:33:13 +0100

    made changes

--- terraform/aws_s3_bucket.tf
+++ terraform/aws_s3_bucket.tf
@@ -20,6 +20,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "aws-s3-bucket-ser
 
 resource "aws_s3_object" "java-jar-file" {
   bucket = aws_s3_bucket.aws-s3-bucket-java-application.id
-  key    = "target/upskilling-7-SNAPSHOT.jar"
-  source = "../target/upskilling-7-SNAPSHOT.jar"
+  key    = "target/upskilling-8-SNAPSHOT.jar"
+  source = "../target/upskilling-8-SNAPSHOT.jar"
 }
