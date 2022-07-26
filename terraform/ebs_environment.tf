bf7b3bf9abb9ec999dce9c986c6f8e9030170ed1
Aaron Stones <aaronstones@macbook-pro.home>
2022-07-26 11:07:41 +0100

    HOTFIX-1: update java versions and secrets name

--- terraform/ebs_environment.tf
+++ terraform/ebs_environment.tf
@@ -1,10 +1,10 @@
 resource "aws_elastic_beanstalk_environment" "aws-ebs-java-upskilling-project-env" {
 
   depends_on = [aws_elastic_beanstalk_application_version.aws-ebs-java-upskilling-ver]
-  name = "aws-ebs-java-upskilling-project"
+  name = var.JUR_VERSION
   application = aws_elastic_beanstalk_application.ebs-app.name
   solution_stack_name = "64bit Amazon Linux 2 v3.2.16 running Corretto 11"
-  cname_prefix = "aws-ebs-java-upskilling-project"
+  cname_prefix = var.JUR_VERSION
   version_label = aws_elastic_beanstalk_application_version.aws-ebs-java-upskilling-ver.name
 
   setting {
