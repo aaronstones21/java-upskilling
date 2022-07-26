39e94c7cccbd3b97fa7f102cae5401c6654be750
Aaron Stones <aaronstones@macbook-pro.home>
2022-07-26 11:17:40 +0100

    HOTFIX-1: update java versions and secrets name

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,6 +15,6 @@ variable "AWS_IAM_USER" {
 
 variable "JUR_VERSION" {
   type = string 
-  default = "aws-ebs-java-upskilling-project-0.0.5-SNAPSHOT"
+  default = "java-project-0.0.5-SNAPSHOT"
   description = "Version of the project to keep the AWS EBS name unique"
 }
